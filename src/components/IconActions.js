54c960c1798700142c10f80573d36a113153b506
Hunain Bin Sajid <hunainbinsajid@gmail.com>
2022-12-15 05:54:23 +0500

    refactor(ui): move top bar menu to dots popup

--- src/components/IconActions.js
+++ src/components/IconActions.js
@@ -1,51 +1,87 @@
-import React from 'react';
-import {View} from 'react-native';
-import {IconButton} from 'react-native-paper';
-import { ROUTE_NAMES } from '../navigation/constants';
+import React from "react";
+import { View } from "react-native";
+import { Button, Menu, IconButton } from "react-native-paper";
+import { ROUTE_NAMES } from "../navigation/constants";
 
 // import {asContactShareable, getContactByAlias, showRel} from '../relationships';
 
 export default function IconActions(...props) {
-//  console.log("IconActions - props",props)
-    const navigation = props[0]["nav"]
-    const add = props[0]["add"]
-    const person = props[0]["person"]
-    const scan = props[0]["scan"]
-    const settings = props[0]["settings"]
-    const workflows = props[0]["workflows"]
-//          <IconButton
-//                icon="plus"
-//                size={28}
-//                color="#e69138"
-//                onPress={() => navigation.navigate(add)}
-//            />
-    return (
-        <View style={{flexDirection: 'row',}}>
+  const [visible, setVisible] = React.useState(false);
+  const openMenu = () => setVisible(true);
+  const closeMenu = () => setVisible(false);
+  //  console.log("IconActions - props",props)
+  const navigation = props[0]["nav"];
+  const add = props[0]["add"];
+  const person = props[0]["person"];
+  const scan = props[0]["scan"];
+  const settings = props[0]["settings"];
+  const workflows = props[0]["workflows"];
+  //          <IconButton
+  //                icon="plus"
+  //                size={28}
+  //                color="#e69138"
+  //                onPress={() => navigation.navigate(add)}
+  //            />
+  return (
+    <View style={{ flex: 1 }}>
+      <Menu
+        visible={visible}
+        onDismiss={closeMenu}
+        contentStyle={{
+          backgroundColor: "#150510",
+          display: "flex",
+          alignItems: "center",
+        }}
+        // style={{backgroundColor: '#150510'}}
+        anchor={
+          <IconButton
+            onPress={openMenu}
+            icon="dots-vertical"
+            size={28}
+            color="#e69138"
+          />
+        }
+      >
+        <Menu.Item
+          onPress={() => console.log("onPress clicked")}
+          style={{ backgroundColor: "#150510" }}
+          title={<IconButton icon="account" size={28} color="#e69138" />}
+        />
+        <Menu.Item
+          onPress={() => {
+            navigation.navigate(ROUTE_NAMES.SCAN_QR_CODE, { type: scan });
+            closeMenu();
+          }}
+          style={{ backgroundColor: "#150510" }}
+          title={
             <IconButton
-                icon="account"
-                size={28}
-                color="#e69138"
-                // onPress={() => showRel(navigation, asContactShareable(getContactByAlias(person)))}
-                onPress={() => console.log('onPress clicked')}
+              icon="qrcode-scan"
+              size={28}
+              color="#e69138"
+              onPress={() => {
+                navigation.navigate(ROUTE_NAMES.SCAN_QR_CODE, { type: scan });
+                closeMenu();
+              }}
             />
-            <IconButton
-                icon="qrcode-scan"
-                size={28}
-                color="#e69138"
-                onPress={() => navigation.navigate(ROUTE_NAMES.SCAN_QR_CODE, {type: scan})}
-            />
-            <IconButton
-                icon="cog-outline"
-                size={28}
-                color="#e69138"
-                onPress={() => navigation.navigate(settings)}
-            />
-            <IconButton
-                icon="sitemap"
-                size={28}
-                color="#e69138"
-                onPress={() => navigation.navigate(workflows)}
-            />
-        </View>
-    )
+          }
+        />
+        <Menu.Item
+          onPress={() => {
+            navigation.navigate(settings);
+            closeMenu();
+          }}
+          title={<IconButton icon="cog-outline" size={28} color="#e69138" />}
+          style={{ backgroundColor: "#150510" }}
+        />
+        <Menu.Item
+          onPress={() => {
+            navigation.navigate(workflows);
+            closeMenu();
+          }}
+          title={<IconButton icon="sitemap" size={28} color="#e69138" />}
+          style={{ backgroundColor: "#150510" }}
+        />
+      </Menu>
+    </View>
+  );
 }
