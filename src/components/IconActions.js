7420fb514a46f743fc14ef13df5e4d6592435fc1
Hunain Bin Sajid <hunainbinsajid@gmail.com>
2022-12-16 19:57:00 +0500

    feat(wip): revamp modals to bottom sheets

--- src/components/IconActions.js
+++ src/components/IconActions.js
@@ -16,38 +16,45 @@ export default function IconActions(...props) {
   const scan = props[0]["scan"];
   const settings = props[0]["settings"];
   const workflows = props[0]["workflows"];
-  //          <IconButton
-  //                icon="plus"
-  //                size={28}
-  //                color="#e69138"
-  //                onPress={() => navigation.navigate(add)}
-  //            />
+
   return (
-    <View style={{ flex: 1 }}>
-      <Menu
-        visible={visible}
-        onDismiss={closeMenu}
-        contentStyle={{
-          backgroundColor: "#000000",
-          display: "flex",
-          alignItems: "center",
+    <View style={{ display: "flex", flexDirection: "row" }}>
+      <IconButton
+        icon="qrcode-scan"
+        size={28}
+        color="#e69138"
+        onPress={() => {
+          navigation.navigate(ROUTE_NAMES.SCAN_QR_CODE, { type: scan });
+          closeMenu();
         }}
-        // style={{backgroundColor: '#000000'}}
-        anchor={
-          <IconButton
-            onPress={openMenu}
-            icon="dots-vertical"
-            size={28}
-            color="#e69138"
+      />
+      <View style={{ display: "flex", alignItems: "flex-end" }}>
+        <Menu
+          visible={visible}
+          onDismiss={closeMenu}
+          contentStyle={{
+            backgroundColor: "#000000",
+            display: "flex",
+            alignItems: "center",
+            borderWidth: 1,
+            borderColor: '#DE984F',
+            borderRadius: 12
+          }}
+          anchor={
+            <IconButton
+              onPress={openMenu}
+              icon="dots-vertical"
+              size={28}
+              color="#e69138"
+            />
+          }
+        >
+          <Menu.Item
+            onPress={() => console.log("onPress clicked")}
+            style={{ backgroundColor: "#000000" }}
+            title={<IconButton icon="account" size={28} color="#e69138" />}
           />
-        }
-      >
-        <Menu.Item
-          onPress={() => console.log("onPress clicked")}
-          style={{ backgroundColor: "#000000" }}
-          title={<IconButton icon="account" size={28} color="#e69138" />}
-        />
-        <Menu.Item
+          {/* <Menu.Item
           onPress={() => {
             navigation.navigate(ROUTE_NAMES.SCAN_QR_CODE, { type: scan });
             closeMenu();
@@ -64,24 +71,25 @@ export default function IconActions(...props) {
               }}
             />
           }
-        />
-        <Menu.Item
-          onPress={() => {
-            navigation.navigate(settings);
-            closeMenu();
-          }}
-          title={<IconButton icon="cog-outline" size={28} color="#e69138" />}
-          style={{ backgroundColor: "#000000" }}
-        />
-        <Menu.Item
-          onPress={() => {
-            navigation.navigate(workflows);
-            closeMenu();
-          }}
-          title={<IconButton icon="sitemap" size={28} color="#e69138" />}
-          style={{ backgroundColor: "#000000" }}
-        />
-      </Menu>
+        /> */}
+          <Menu.Item
+            onPress={() => {
+              navigation.navigate(settings);
+              closeMenu();
+            }}
+            title={<IconButton icon="cog-outline" size={28} color="#e69138" />}
+            style={{ backgroundColor: "#000000" }}
+          />
+          <Menu.Item
+            onPress={() => {
+              navigation.navigate(workflows);
+              closeMenu();
+            }}
+            title={<IconButton icon="sitemap" size={28} color="#e69138" />}
+            style={{ backgroundColor: "#000000" }}
+          />
+        </Menu>
+      </View>
     </View>
   );
 }
