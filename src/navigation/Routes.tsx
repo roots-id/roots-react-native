f5c3481d8e5838af56bfb6216c3ec9a3631db87e
AlexAndrei98 <alexandrei1998@hotmail.it>
2023-03-12 13:32:11 -0500

    remove workflow

--- src/navigation/Routes.tsx
+++ src/navigation/Routes.tsx
@@ -36,69 +36,6 @@ export default function Routes() {
   );
 
   useEffect(() => {
-    // localStorageService.clear();
-
-    //should check for messaes from mediator 
-  //   const FakeConnection = async () => {
-  //     const RootsIDClient = await dispatch(addContact({
-  //       _id : 'RootsIDClient',
-  //       displayName: 'Activity Log',
-  //       displayPictureUrl: 'https://source.unsplash.com/500x500/?soccer,logo'
-  //     })).payload;
-  //     console.log('RootsIDClient', RootsIDClient);
-
-  //       const contact = await dispatch(addContact({
-  //         _id : 'rootUser',
-  //         displayName: 'Activity Log',
-  //         displayPictureUrl: 'https://source.unsplash.com/500x500/?logo,soccer'
-  //       })).payload;
-  //       console.log('contact', contact);
-
-  //       const chat = await dispatch(initiateChat({
-  //         chatId : contact._id//is the id from above eg: rootUser}
-  //       }
-  //       )).payload;
-  //       console.log('chat', chat);
-
-  //       const message = await dispatch(sendMessageToChat({
-  //         chatId: contact._id,
-  //         message: 'Welcome to the Activity Log',
-  //         senderId: RootsIDClient._id,
-  //         type: MessageType.TEXT
-  //       })).payload;
-
-
-  //       dispatch(
-  //     addMessage({
-  //       chatId: contact._id,
-  //       message: addMsgMetadata(
-  //         contact._id,
-  //         'RootsIDClient',
-  //         `You have issued a verifiable credential!`,
-  //         MessageType.CREDENTIAL_OFFER,
-  //         false,
-  //         { credential: {
-  //           is_offer: 'offered',
-  //           image_url: 'https://source.unsplash.com/500x500/?soccer,logo',
-  //           id: '435hk-34jkh5-n3l4kt-hn4w5ltu',
-  //           type: ['VerifiableCredential', 'UniversityDegreeCredential'],
-  //           issuer: 'https://example.edu/issuers/14',
-  //           issuanceDate: '2020-03-10T04:24:12.164Z',
-  //           credentialSubject: {
-  //             id: 'did:example:ebfeb1f712ebc6f1c276e12ec21',
-  //             degree: {
-  //               type: 'BachelorDegree',
-  //               name: 'Bachelor of Science and Arts',
-  //             },
-  //           }
-  //         } }
-  //       ),
-  //     })
-  //   );
-  //     setLoggedIn(true);
-  //   };
-  //   FakeConnection();
-  // }, []);
   setLoggedIn(true);
   }, []);
 
